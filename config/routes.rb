d40220412f0b81e336e9f0882186a9225ce2ecfd
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-15 11:14:37 +0200

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # verb "url", to: "controller#action"
 
   # A visitor can see the list of all restaurants
-  GET 'restaurants', to: "restaurants#index"
+  get 'restaurants', to: "restaurants#index"
 
   # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
-  GET "restaurants/new", to: "restaurants#new"
-  POST "restaurants", to: "restaurants#create"
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
 
   # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
-  GET "restaurants/:id", to: "restaurants#show"
+  get "restaurants/:id", to: "restaurants#show"
 
   # A visitor can add a new review to a restaurant
-  GET "restaurants/38/reviews/new"
-  POST "restaurants/38/reviews"
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
 end
