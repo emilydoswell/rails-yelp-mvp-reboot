22b3c3bd4ffffbea66bb1dc8347c52a17311f867
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-18 11:03:10 +0200

    rails

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,15 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+    @review.restaurant = @restaurant
   end
 
   def create
-    @review = Review.new(review_params)
-
     # we need `restaurant_id` to associate review with corresponding restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
+    
+    @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
