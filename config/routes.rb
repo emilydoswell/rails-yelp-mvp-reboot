8cd8e166ebc61929335b9239b8a43debe35610aa
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-18 18:46:26 +0200

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,6 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
+  resources :reviews, only: [:destroy]
 end
 
